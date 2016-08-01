342f9553b64cba721413880ea5aa8714d8e83406
gatherer <hunteralbaugh@gmail.com>
2016-08-01 01:09:00 +0000

    Added current bearing and new bearing.

--- nsp_run.py
+++ nsp_run.py
@@ -12,28 +12,33 @@ class Nsp(object):
         self.loadSettings()
         self.nanoPos = self.settings["nanoPos"]
         self.lastPos = self.settings["lastPos"]
+        self.currentBearing = self.settings["stepperBearing"]
         self.target = [float(raw_input("Lat?")), float(raw_input("Lon?"))]
     
     def loadSettings(self):
         self.settings = json.load(open(nsp_settings_path))
     
     def calcBearing(self):
-        return gps.calculate_initial_compass_bearing(self.nanoPos,self.target)
+        self.newBearing = gps.calculate_initial_compass_bearing(self.nanoPos,self.target)
         
     def displayInfo(self):
-        print "NS @ - ", self.nanoPos
-        print "Last Target @ - ", self.lastPos
-        print "Target @ ", self.target
+        print "NS @ ", self.nanoPos
+        print "Last Target @ ", self.lastPos
+        print "Current Bearing @ ", self.currentBearing
+        print ""
+        print "New Target @ ", self.target
+        print "New Bearing @ ", self.newBearing
     
     
 def saveSettings(nspObj):
     nspObj.settings["lastPos"] = nspObj.target
+    nspObj.settings["stepperBearing"] = nspObj.newBearing
     with open(nsp_settings_path,"w") as f:
         f.write(json.dumps(nspObj.settings))
     
     
 myNsp = Nsp()
-
+myNsp.calcBearing()
 myNsp.displayInfo()
 
 
