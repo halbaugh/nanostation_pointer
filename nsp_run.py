8ce931d32bbaab46c1a0b9e75587c79af1d0068f
gatherer <hunteralbaugh@gmail.com>
2016-08-03 03:13:43 +0000

    Haleys help with direction logic.

--- nsp_run.py
+++ nsp_run.py
@@ -5,7 +5,7 @@ import sys
 import gpsMath as gps
 import RPi.GPIO as GPIO
 import time
-import stepper
+import stepper as step
 
 nsp_settings_path = os.path.dirname(os.path.abspath(__file__)) + "/nsp_settings.json"
 
@@ -34,14 +34,14 @@ def saveSettings(nspObj):
     nspObj.settings["lastPos"] = nspObj.target
     with open(nsp_settings_path,"w") as f:
         f.write(json.dumps(nspObj.settings))
-    
+def turnRight():    
 
-    
 myNsp = Nsp()
-myMotor = stepper.Stepper()
+myMotor = step.Stepper()
 
 myNsp.displayInfo()
 newBearing = myNsp.calcBearing()
+
 print "New Bearing - ", newBearing
 '''
 delay = raw_input("Delay between steps (milliseconds)?")
@@ -50,7 +50,12 @@ forward(int(delay) / 1000.0, int(steps))
 steps = raw_input("How many steps backwards? ")
 backwards(int(delay) / 1000.0, int(steps))
 '''
-
+'''
+if((newBearing-bearing) < 180):
+    turnRight(newBearing-bearing)
+else:
+    turnLeft(newBearing-bearing)
+'''
 #saves settings back to .json
 
 atexit.register(saveSettings, myNsp)
